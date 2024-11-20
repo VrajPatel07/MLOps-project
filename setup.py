5c4c2421f94d7817d7f822863fe9b55ef2806990
VrajPatel07 <vrajkpatel6119@gmail.com>
2024-11-20 20:04:11 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "src",
+    version = "0.0.1",
+    author = "Vraj Patel",
+    author_email = "vrajpatel6119@gmail.com",
+    packages = find_packages()
+)
